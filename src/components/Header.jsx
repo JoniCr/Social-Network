ec43374edc12ea69e91e179cce90cfdeb1b08b9c
JoniCR <business_evgeniy_04@mail.ru>
2020-04-08 12:46:59 +0500

    first-commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header parent">
+            <img src="https://image.flaticon.com/icons/svg/124/124029.svg" alt="logotype" className="logo" />
+        </header>
+    );
+}
+
+
+export default Header;
