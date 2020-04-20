f3b1007378ccaeef479ee619ef9b310af514b376
JoniCR <business_evgeniy_04@mail.ru>
2020-04-20 09:58:07 +0500

    'pagination_v2'

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,22 +6,38 @@ import {NavLink} from "react-router-dom";
 
 const Users = props => {
     const pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
-    let pages = [];
-    for (let i = 1; i <= pagesCount; i++) {
-        pages.push(i);
-    }
+    const page = props.currentPage;
+    let chosenPage = page;
+
+    const onPageLess = () => {
+        if (chosenPage === 1) {
+            return false
+        } else {
+            chosenPage -= 1;
+            props.onPageChange(chosenPage);
+        }
+    };
+
+    const onPageNext = () => {
+        if (chosenPage === pagesCount) {
+            alert('There is no more users)')
+        } else {
+            chosenPage += 1;
+            props.onPageChange(chosenPage);
+        }
+    };
+
     return (
         <section className="Users">
-            {
 
-                pages.map(page => {
-                    return props.currentPage === page ?
-                        <span className={`${Styles.selectedPage} ${Styles.pagination}`} key={page}
-                              onClick={() => props.onPageChange(page)}>{page}</span> :
-                        <span key={page} className={Styles.pagination}
-                              onClick={() => props.onPageChange(page)}>{page}</span>
-                })
-            }
+            <div>
+                <img className={`${Styles.arrow_img} ${Styles.arrow_left}`} onClick={onPageLess}
+                     src="https://image.flaticon.com/icons/svg/271/271228.svg" alt="arrow left"/>
+                <span>{page}</span>
+                <img className={Styles.arrow_img} onClick={onPageNext}
+                     src="https://image.flaticon.com/icons/svg/271/271228.svg" alt="arrow left"/>
+            </div>
+
 
             {
 
@@ -37,7 +53,7 @@ const Users = props => {
                             </div>
                             <div className="info">
                                 <NavLink to={`/profile/${user.id}`}>
-                                <span>{user.name}</span>
+                                    <span>{user.name}</span>
                                 </NavLink>
                                 <span>{"user.location.country"},{"user.location.city"}</span>
                                 <span>{user.status}</span>
